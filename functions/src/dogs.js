c1f5860e9ab2327360623e1abf8c36a14aa1a6c8
Alicia Myrie <aliciamyrie@gmail.com>
2022-08-22 15:37:40 -0400

    added Indigo The Great

--- functions/src/dogs.js
+++ functions/src/dogs.js
@@ -2,6 +2,7 @@ export function getDogs(req, res) {
   res.send([
     { name: "Ryder", age: 4, breed: "Terrier", gender: "MN" },
     { name: "Duke", age: 2, breed: "Rotweiler", gender: "M" },
+    { name: "Indigo", age: 10, breed: "Great Dane/Lab MIx", gender: "F"}
   ]);
 }
 
